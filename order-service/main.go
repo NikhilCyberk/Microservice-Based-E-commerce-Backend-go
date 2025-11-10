735468f33015348eb77e34b6d682de62a7e12990
nikhil kumar <111166860+NikhilCyberk@users.noreply.github.com>
2025-11-10 08:25:55 +0530

    Create main.go

---
+++ order-service/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"time"
+
+	pb "order-service/order-service/proto"
+	"order-service/service"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+    userServiceURL := os.Getenv("USER_SERVICE_URL")
+    if userServiceURL == "" {
+        userServiceURL = "localhost:50051"
+    }
+
+    productServiceURL := os.Getenv("PRODUCT_SERVICE_URL")
+    if productServiceURL == "" {
+        productServiceURL = "localhost:50052"
+    }
+
+    rabbitMQURL := os.Getenv("RABBITMQ_URL")
+    if rabbitMQURL == "" {
+        rabbitMQURL = "amqp://admin:admin@localhost:5672/"
+    }
+
+    // Wait for dependencies to be ready
+    log.Println("Waiting for dependencies to be ready...")
+    time.Sleep(5 * time.Second)
+
+    orderService, err := service.NewOrderService(userServiceURL, productServiceURL, rabbitMQURL)
+    if err != nil {
+        log.Fatalf("Failed to create order service: %v", err)
+    }
+
+    lis, err := net.Listen("tcp", ":50053")
+    if err != nil {
+        log.Fatalf("Failed to listen: %v", err)
+    }
+
+    s := grpc.NewServer()
+    pb.RegisterOrderServiceServer(s, orderService)
+
+    log.Printf("Order service listening on :50053")
+    log.Printf("Connected to User Service: %s", userServiceURL)
+    log.Printf("Connected to Product Service: %s", productServiceURL)
+    log.Printf("Connected to RabbitMQ: %s", rabbitMQURL)
+
+    if err := s.Serve(lis); err != nil {
+        log.Fatalf("Failed to serve: %v", err)
+    }
+}
